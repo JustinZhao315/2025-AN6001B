e01e8052597ba9ed24079e8347b650b42d4dd6ac
Justin Zhao <ZHAO0527@e.ntu.edu.sg>
2025-11-21 07:11:14 +0000

    1

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ from flask import Flask,render_template,request
 app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
-
 def index():
     return(render_template("index.html"))
 
 @app.route("/main",methods=["GET","POST"])
-
 def main():
     q=request.form.get("q")
     return(render_template("main.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    q=request.form.get("q")
+    return(render_template("dbs.html"))
 
 if __name__=="__main__":
     app.run()
